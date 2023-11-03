3fc566b1c1dd7cc1e40821495d931a937c7e333e
Vincent Perez <perezv815@gmail.com>
2023-11-03 21:00:47 +0100

    14.5

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,26 +1,56 @@
-// NOTE: the size of an OpCode is 1 byte. It can be checked with mem::size_of
+use crate::value::{print_value, Value};
+use std::convert::TryFrom;
+
 #[derive(Debug)]
 pub enum OpCode {
+    OpConstant,
     OpReturn,
 }
 
+// allows cast from u8 to OpCode
+impl TryFrom<u8> for OpCode {
+    type Error = ();
+
+    fn try_from(v: u8) -> Result<Self, Self::Error> {
+        match v {
+            x if x == OpCode::OpConstant as u8 => Ok(OpCode::OpConstant),
+            x if x == OpCode::OpReturn as u8 => Ok(OpCode::OpReturn),
+            _ => Err(()),
+        }
+    }
+}
+
 pub struct Chunk {
-    code: Vec<OpCode>,
+    code: Vec<u8>,
+    constants: Vec<Value>,
 }
 
 impl Chunk {
     pub fn new() -> Chunk {
-        Chunk { code: Vec::new() }
+        Chunk {
+            code: Vec::new(),
+            constants: Vec::new(),
+        }
     }
 
     pub fn count(&self) -> usize {
         self.code.len()
     }
 
-    pub fn write(&mut self, op_code: OpCode) {
+    pub fn write(&mut self, op_code: u8) {
         self.code.push(op_code);
     }
 
+    pub fn add_constant(&mut self, value: Value) -> u8 {
+        self.constants.push(value);
+        (self.constants.len() - 1)
+            .try_into()
+            .expect("Constant index didn't fit in byte")
+    }
+}
+
+/// debug implementation
+impl Chunk {
     pub fn disassemble(&self, name: &str) {
         println!("== {} ==", name);
         let mut offset: usize = 0;
@@ -31,14 +61,23 @@ impl Chunk {
     fn disassemble_instruction(&self, offset: usize) -> usize {
         print!("{:04} ", offset);
 
-        let instruction = &self.code[offset];
+        let instruction: &OpCode = &self.code[offset].try_into().expect("Could not decode byte");
         match instruction {
-            OpCode::OpReturn => Chunk::simple_instruction("OP_RETURN", offset),
+            OpCode::OpReturn => self.simple_instruction("OP_RETURN", offset),
+            OpCode::OpConstant => self.constant_instruction("OP_CONSTANT", offset),
         }
     }
 
-    fn simple_instruction(name: &str, offset: usize) -> usize {
+    fn simple_instruction(&self, name: &str, offset: usize) -> usize {
         println!("{}", name);
         offset + 1
+    }
+
+    fn constant_instruction(&self, name: &str, offset: usize) -> usize {
+        let constant_addr = self.code[offset + 1];
+        print!("{} {:04} '", name, constant_addr);
+        print_value(self.constants[constant_addr as usize]);
+        println!("'");
+        offset + 2
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use rox::chunk::{Chunk, OpCode};
 
 fn main() {
     let mut chunk = Chunk::new();
-    chunk.write(OpCode::OpReturn);
+    let constant = chunk.add_constant(1.2);
+    chunk.write(OpCode::OpConstant as u8);
+    chunk.write(constant);
+    chunk.write(OpCode::OpReturn as u8);
     chunk.disassemble("test chunk")
 }
